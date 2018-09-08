6e9584e56b741d6a975d059556b2387e9879f26b
Chia-Chi <d8958101@gmail.com>
2018-09-08 15:58:25 +0800

    先拆開Gloria crawler到另一個module

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
-# 引入 requests 模組
-import requests
-# 使用 GET 方式下載普通網頁
-resp = requests.get('https://www.gloriatour.com.tw/EW/GO/GroupList.asp')
+from Crawlers import GloriaCrawler
+#from Crawlers.GloriaCrawler import GloriaCrawler
+objG = GloriaCrawler()
+objG.GO()
 
-if resp.status_code == requests.codes.ok:
-  print("OK")
-else:
-  print("There is a problem!")
-  exit()
-html = resp.text
-#https://regex101.com/r/XHuNJH/1
-pattern = r'<div class=\"product_name\">.*?<span class=\"product_num\">[\d\w]+</span>\s*\r\n\s*(?P<TourName>.*?)\s*\r\n\s*<div class=\"product_tag\">.*?<div class=\"product_days\">(?P<Days>\d+)天.*?<div class=\"product_date normal\">(?P<Date>\d{4}/\d{2}/\d{2}).*?售價\$<strong>(?P<Money>[0-9,]+)</strong>.*?機位<\/span><span class=\"number\">(?P<Total>\d+)</span>.*?可售<\/span><span class=\"number\">(?P<Available>\d+)</span><\/div>'
-
-
-import re
-# DOTALL：就是csharp裡面的singleline
-pattern = re.compile(pattern, re.DOTALL)
-for m in pattern.finditer(html):
-  print(m.group('TourName'))  
-  print(m.group('Days'))  
 print('program end')
