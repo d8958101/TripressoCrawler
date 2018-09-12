92d0b561a5fedd47b885acab75305469e0679c81
Chia-Chi <d8958101@gmail.com>
2018-09-12 16:01:05 +0800

    修正原本假的multi threading 變成真的了 multi threading最佳化完畢！

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from Crawlers import GloriaCrawler, OrangeCrawler
-import threading
+from concurrent.futures import ThreadPoolExecutor
 import time
 
 objG = GloriaCrawler('Gloria', 'https://www.gloriatour.com.tw/EW/GO/GroupList.asp' \
 , 'https://www.gloriatour.com.tw/EW/Services/SearchListData.asp' \
 , 3)
-# 建立一個子執行緒並立刻執行
-t = threading.Thread(target = objG.Go())
-
 
 objO = OrangeCrawler('Orange', 'http://www.orangetour.com.tw/EW/GO/GroupList.asp' \
 , 'http://www.orangetour.com.tw/EW/Services/SearchListData.asp' \
 , 3)
-# 建立一個子執行緒並立刻執行
-t1 = threading.Thread(target = objO.Go())
+
+with ThreadPoolExecutor() as executor:
+    executor.submit(objG.Go())
+    executor.submit(objO.Go())
 
 # 主執行緒繼續執行自己的工作
 for i in range(10000):
